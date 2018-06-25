5ce425e714211362ee2f615ca989ffdc0c528f85
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2018-06-25 21:53:32 +0200

    check if number is a palindrome using O(1) memory

---
+++ src/ch5PrimitiveTypes/IsNumberPalindrome.kt
@@ -0,0 +1,35 @@
+package ch5PrimitiveTypes
+
+import kotlin.math.log10
+
+fun main(args: Array<String>) {
+    val testNumber1 = 121
+    val testNumber2 = 122
+    val testNumber3 = 2222
+
+    println(isPalindrome(testNumber1))
+    println(isPalindrome(testNumber2))
+    println(isPalindrome(testNumber3))
+}
+
+fun isPalindrome(num: Int): Boolean{
+    var number = num
+    val numberOfDigits = log10(number.toDouble()).toInt() + 1
+    var mask = Math.pow(10.toDouble(), (numberOfDigits - 1).toDouble())
+
+    repeat(numberOfDigits/2, {
+        val mostLeft = (number / mask).toInt()
+        val mostRight = number.rem(10)
+
+        if(mostLeft != mostRight){
+            return false
+        }
+
+        number = number.rem(mask).toInt()
+        number /= 10
+        mask /= 100
+    })
+
+    return true
+}
+
